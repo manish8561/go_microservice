27855912cfa27477e89fb7125bc174875aa38670
akhilxyz <akhilchoudharymailbox@gmail.com>
2022-03-10 17:40:47 +0530

    fix issues

--- contract_service/src/model/schema/farms.ts
+++ contract_service/src/model/schema/farms.ts
@@ -16,40 +16,40 @@ class farmsSchema extends Schema {
             pid: { type: Number },
             address: { type: String },
             name: { type: String },
-            token_type:{type: String},
-            deposit_token:{type: String},
-            masterchef:{type: String},
-
-
-
+            token_type: { type: String },
+            deposit_token: { type: String },
+            masterchef: { type: String },
             tvl_staked: { type: Number },// value should be in USD
             daily_apr: { type: Number },
             daily_apy: { type: Number },
             status: { type: String },
+            token0: { type: Object },
+            token1: { type: Object },
 
+            router: { type: String },
+            weth: { type: String },
+            stake: { type: String },
+            ac_token: { type: String },
+            reward: { type: String },
+            bonus_multiplier: { type: Number },
+            token_per_block: { type: Number },
 
-
-
-            token0: {type:Object},
-            token1:{type:Object},
-
-	Router             string`bson:"router" json:"router"`
-	Weth               string`bson:"weth" json:"weth"`
-	Stake              string`bson:"stake" json:"stake"`       //staking contract address
-	AC_Token           string`bson:"ac_token" json:"ac_token"` //autocompound token
-	Reward             string`bson:"reward" json:"reward"`     //cake address
-	Bonus_Multiplier   int`bson:"bonus_multiplier" json:"bonus_multiplier"`
-	Token_Per_Block    int`bson:"token_per_block" json:"token_per_block"`
-	Source             string`bson:"source" json:"source"`
-	Source_Link        string`bson:"source_link" json:"source_link"`
-	Autocompound_Check bool`bson:"autocompound_check" json:"autocompound_check"`
-	Weekly_APY         float64`bson:"weekly_apy" json:"weekly_apy"`
-	Yearly_APY         float64`bson:"yearly_apy" json:"yearly_apy"`
-	Price_Pool_Token   float64`bson:"price_pool_token" json:"price_pool_token"`
-	Yearly_Swap_Fees   float64`bson:"yearly_swap_fees" json:"yearly_swap_fees"`
-	Gauge_Info         string`bson:"gauge_info" json:"gauge_info"`
+            source: { type: String },
+            source_link: { type: String },
+            autocompound_check: { type: Boolean },
+            // tvl_staked: { type: Number },
+            // daily_apr: { type: Number },
+            // daily_apy: { type: Number },
+            weekly_apy: { type: Number },
+            yearly_apy: { type: Number },
+            price_pool_token: { type: Number },
+            yearly_swap_fees: { type: Number },
+            // token0: { type: Object },
+            // token1: { type: Object },
+            gauge_info: { type: String }
         }, { timestamps: false, strict: false });
-this.objectSchema.index({  address: 1, status:1 })
+
+        this.objectSchema.index({ address: 1, status: 1 });
     }
 }
 
--- contract_service/src/modules/contract/contract.model.ts
+++ contract_service/src/modules/contract/contract.model.ts
@@ -2,75 +2,34 @@ import BaseModel from "../../model/base.model";
 import masterChefHelper from "./masterChef.helper";
 import farms from "../../model/schema/farms";
 import { Responses } from "../../helpers";
-import axios from "axios";
+// import axios from "axios";
+// import { initParams } from "request-promise";
 
 
 class farmModel extends BaseModel {
   constructor() {
     super();
   }
-
-
   public async getAprValue(response: any) {
-    const farmData: any = await axios.get(`${process.env.FARM_API_URL}farm?page=1&limit=10&status=active`);
-
-    if (farmData.status === 200 && farmData.data.data.length > 0) {
-      try {
-
-        if (farmData && farmData.data.data.length > 0) {
-          let newData: any = [];
-          for (let it  of farmData.data.data){
-            const { masterchef, deposit_token }: any = it;
-            // console.log("masterChecfffffffff", masterchef, deposit_token)
-            const calApr: any = await masterChefHelper.calculateAPRValue(masterchef, deposit_token);
-            it.daily_apr = calApr
-            // if (it.daily_apr) {
-            //   console.log("april", calApr)
-
-            //   it.daily_apr = calApr
-            // }
-            // console.log("ittttttttttttt", it)
-
-            newData.push(it);
-            console.log("newDataaaaaaaaaaa1", newData)
-
-          }
-        
-          console.log("newDataaaaaaaaaaa2", newData)
-
+    try {
+      const farmData: any = await farms.find({})
+      const newArr: any = [];
+      if (farmData.length > 0) {
+        for (let it of farmData) {
+          const { masterchef, deposit_token }: any = it;
+          const calApr: any = await masterChefHelper.calculateAPRValue(masterchef, deposit_token);
+          it.daily_apr = calApr
+          newArr.push(it)
         }
-
-
-        // await farms.insertMany(farmData.data.data, { ordered: false }).catch(err => {
-        //   console.error(err);
-        // })
-
-
-
-        const farmList = await farms.find({});
-
-        // if (farmList && farmList.length > 0) {
-        //   farmList.map((it: any) => {
-        //     const mC : any = it.masterchef
-        //     console.log('HERE', it, i)
-        //   })
-        // }
-
-        // console.log("FARMMMMMMMMMMMMMMM", farmList);
-        // const calApr: any = await masterChefHelper.calculateAPRValue(abi, ContarctAddess);
-        // const data: any = await new farms({ apr: calApr }).save();
-        return farmList
-      } catch (error) {
-        throw Responses.error(response, { message: error });
+        await farms.insertMany(newArr, { ordered: false }).catch(err => {
+          console.error(err);
+        })
       }
-
     }
-
-
-    // }
+    catch (error) {
+      throw Responses.error(response, { message: error });
+    }
   }
-
-
 }
 
 export default new farmModel();

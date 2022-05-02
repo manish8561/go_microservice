f698f618f40ff89c63974f894b1d2fd3e6574dbc
manish <manish.sharma@antechindia.com>
2022-05-02 11:55:58 +0530

    fix

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@ docker-compose down
 git pull origin main
 docker-compose build
 docker-compose up -d redis gateway_service
-docker-compose up -d user farm contract_service
+docker-compose up -d user farm contract_service governance

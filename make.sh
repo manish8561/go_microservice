4500d7d88a683c6f297c82ba685ac3524f02f4e3
manish <manish.sharma@antechindia.com>
2022-06-15 11:53:33 +0530

    fix the CD file

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 docker-compose down
-# git pull origin main
-git pull origin stage-v2
+git pull origin main
+# git pull origin stage-v2
 docker-compose build
 docker-compose up -d redis gateway_service
 docker-compose up -d user farm contract_service governance event_service
